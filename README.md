db4bdfbcf053b91098852c42f87775f9a2a8bab9
Kongpon Charanwattanakit <jackykongpon@gmail.com>
2020-08-21 11:36:40 +0700

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+@kykungz/swagger-to-ts
+======================
+
+
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/@kykungz/swagger-to-ts.svg)](https://npmjs.org/package/@kykungz/swagger-to-ts)
+[![Downloads/week](https://img.shields.io/npm/dw/@kykungz/swagger-to-ts.svg)](https://npmjs.org/package/@kykungz/swagger-to-ts)
+[![License](https://img.shields.io/npm/l/@kykungz/swagger-to-ts.svg)](https://github.com/kykungz/swagger-to-ts/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g @kykungz/swagger-to-ts
+$ swagger-to-ts COMMAND
+running command...
+$ swagger-to-ts (-v|--version|version)
+@kykungz/swagger-to-ts/0.0.0 darwin-x64 node-v10.20.1
+$ swagger-to-ts --help [COMMAND]
+USAGE
+  $ swagger-to-ts COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.ts
@@ -0,0 +1,76 @@
+import { Command, flags } from '@oclif/command'
+import swaggerToTS from '@manifoldco/swagger-to-ts'
+import fetch from 'node-fetch'
+import fs from 'fs'
+import path from 'path'
+import chalk from 'chalk'
+
+class SwaggerToTs extends Command {
+  static description = 'Generate TypeScript types from Swagger OpenAPI specs'
+
+  static examples = [
+    `$ npx @kykungz/swagger-to-ts http://localhost:5000/doc-json --output schema.ts --shallow`,
+    `$ npx @kykungz/swagger-to-ts schema.yaml --output schema.ts --shallow`,
+  ]
+
+  static flags = {
+    version: flags.version({ char: 'v' }),
+    help: flags.help({ char: 'h' }),
+
+    output: flags.string({
+      char: 'o',
+      description: 'Location of the output file',
+      default: 'schema.ts',
+    }),
+
+    shallow: flags.boolean({
+      char: 's',
+      description: 'Add shallow TypeScript interfaces',
+    }),
+  }
+
+  static args = [{ name: 'path' }]
+
+  async run() {
+    const { args, flags } = this.parse(SwaggerToTs)
+    const timeStart = process.hrtime()
+
+    let json = null
+    let output = ''
+
+    this.log(chalk.yellow(`\n# 🤞 Loading spec from ${args.path}...`))
+
+    if ((args.path as string).startsWith('http')) {
+      const res = await fetch('http://localhost:5000/doc-json')
+      json = await res.json()
+      output = swaggerToTS(json)
+    } else {
+      json = JSON.parse(
+        fs.readFileSync(path.join(process.cwd(), args.path), 'utf8'),
+      )
+
+      output = swaggerToTS(json)
+    }
+
+    if (flags.shallow) {
+      const shallow = Object.keys(json.components.schemas)
+        .map((dto) => `export type ${dto} = components['schemas']['${dto}']`)
+        .join('\n\n')
+
+      output += '\n' + shallow
+    }
+
+    fs.writeFileSync(path.join(process.cwd(), flags.output), output)
+
+    const timeEnd = process.hrtime(timeStart)
+    const time = timeEnd[0] + Math.round(timeEnd[1] / 1e6)
+
+    this.log(
+      chalk.green(
+        `# 🚀 ${args.path} -> ${chalk.bold(flags.output)} [${time}ms]`,
+      ),
+    )
+  }
+}
+
+export = SwaggerToTs
